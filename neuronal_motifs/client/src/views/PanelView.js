37fb0ec54b3245fc1dc82f640c850a6050a8345c
Simon Warchol <simonwarchol@g.harvard.edu>
2022-04-07 11:38:21 -0400

    Fixing deprecation error around references

---
+++ neuronal_motifs/client/src/views/PanelView.js
@@ -0,0 +1,18 @@
+import React, {useState, useEffect} from 'react';
+import Draggable from 'react-draggable';
+import './DraggableView.css'
+import MotifPanel from "../components/MotifPanel";
+
+function PanelView() {
+    const nodeRef = React.useRef(null);
+    return (
+        <Draggable handle=".handle" nodeRef={nodeRef}>
+            <div className='drag-overlay' ref={nodeRef}>
+                <MotifPanel/>
+            </div>
+        </Draggable>
+
+    );
+}
+
+export default PanelView;
