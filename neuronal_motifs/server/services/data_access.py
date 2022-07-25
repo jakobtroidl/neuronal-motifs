7de99e01fc1429c6bc59b382df9fc91975f4a7f6
Jakob Troidl <jakob.troidl@googlemail.com>
2022-07-25 11:46:58 -0400

    optimized neuron download

--- neuronal_motifs/server/services/data_access.py
+++ neuronal_motifs/server/services/data_access.py
@@ -1,6 +1,9 @@
+from pathlib import Path
+
 import navis
 import navis.interfaces.neuprint as neu
 import networkit as nk
+import pickle as pkl
 
 from neuronal_motifs.server.models.neuron import Neuron
 from neuronal_motifs.server.utils.authentication import *
@@ -14,15 +17,59 @@ class DataAccess:
         n = navis.example_neurons(n=1, kind='skeleton')
         test = ''
 
+    def load_neuron_from_cache(self, neuron_id):
+        """
+        Loads neuron from cache, if exists. Returns None otherwise
+        @param neuron_id: int
+        @return: Neuron skeleton (pd.DataFrame)
+        """
+        filepath = Path("cache/data/neurons/" + str(neuron_id) + ".pkl")
+        neuron = None
+        if filepath.is_file():
+            # load neuron from filepath
+            with open(filepath, 'rb') as f:
+                neuron = pkl.load(f)
+                f.close()
+        return neuron
+
+
+    def dump_neurons_to_cache(self, neurons):
+        """
+        Dumps a list of neurons to cache
+        @param neurons: [int] list of neuron ids
+        """
+        for neuron in neurons:
+            with open('cache/data/neurons/' + str(neuron.id) + '.pkl', 'wb') as f:
+                pkl.dump(neuron, f)
+            f.close()
+
     def get_neurons(self, body_ids):
         """
         @param body_ids: array of neuron body ids
         @return: dict of Neuron objects
         """
-        print('Download Neurons')
-        skeletons = neu.fetch_skeletons(x=body_ids, with_synapses=True, parallel=True)
-        neurons = []
-        for skel in skeletons:
+        nl = navis.NeuronList([])  # list of neuron objects already in cache
+        neurons_to_download = []  # list of neuron ids that have yet to be downloaded
+        for id in body_ids:
+            neuron = self.load_neuron_from_cache(id)
+            if neuron is None:
+                neurons_to_download.append(id)
+            else:
+                # download neuron
+                nl.append(neuron)
+
+        print("{} neurons loaded from cache".format(len(nl)))
+        print("{} neurons to download".format(len(neurons_to_download)))
+
+        if len(neurons_to_download) > 0:
+            skeletons = neu.fetch_skeletons(x=neurons_to_download, with_synapses=True, parallel=True)
+            self.dump_neurons_to_cache(skeletons)
+            nl.append(skeletons)
+
+        print("Download. Done.")
+
+        output = []
+        for skel in nl:
             healed_skel = navis.heal_skeleton(skel)
 
             # extract method
@@ -31,8 +78,8 @@ class DataAccess:
             nk_graph.indexEdges()
 
             neuron = Neuron(id=skel.id, skeleton=healed_skel, skel_graph=nk_graph)
-            neurons.append(neuron)
-        return neurons
+            output.append(neuron)
+        return output
 
     def get_synapses(self, from_neurons, to_neighbors):
         """
