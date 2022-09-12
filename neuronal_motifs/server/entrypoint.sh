b7acf36280eb956fad07a1953ce90c2e105d7ca6
Simon Warchol <simonwarchol@g.harvard.edu>
2022-09-12 09:39:07 -0400

    Removing entrypoint

--- neuronal_motifs/server/entrypoint.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash --login
-# The --login ensures the bash configuration is loaded,
-conda init bash
-set +euo pipefail
-conda activate neuronal-motifs
-# enable strict mode:
-set -euo pipefail
-python main.py
