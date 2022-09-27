459e81a621746eda03ba8669ab7b969962a50e6f
Jakob Troidl <jakob.troidl@googlemail.com>
2022-09-27 16:46:55 -0400

    add Cytoscape for graph drawing

--- neuronal_motifs/client/src/components/GraphSummary.js
+++ neuronal_motifs/client/src/components/GraphSummary.js
@@ -1,9 +1,18 @@
 import DragHandleIcon from "@mui/icons-material/DragHandle";
 import React from "react";
 import "./GraphSummary.css";
+import CytoscapeComponent from "react-cytoscapejs";
 
 function GraphSummary() {
   const id = "graph-summary-div";
+
+  const elements = [
+    { data: { id: "one", label: "Node 1" }, position: { x: 0, y: 0 } },
+    { data: { id: "two", label: "Node 2" }, position: { x: 100, y: 0 } },
+    {
+      data: { source: "one", target: "two", label: "Edge from Node1 to Node2" },
+    },
+  ];
 
   return (
     <div id={id}>
@@ -14,8 +23,11 @@ function GraphSummary() {
         <div className="item title-wrapper">
           <span>Graph Summary</span>
         </div>
-        <div id="graph" className="item">
-          <h2>here we go</h2>
+        <div id="graph">
+          <CytoscapeComponent
+            elements={elements}
+            style={{ width: "100%", height: "100%" }}
+          />
         </div>
       </div>
     </div>
