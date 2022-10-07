402cc06f7a23e2af3b0a040c0b409290f10fdc59
jinhan <choi.jinhan@gmail.com>
2022-10-07 18:20:19 -0400

    (#56) renaming pickle's module name and upload to the cloud storage

---
+++ neuronal_motifs/server/services/change_pickle_classname.py
@@ -0,0 +1,40 @@
+import pickle
+from pathlib import Path
+from utils.authentication import get_gcloud_storage_bucket
+from params import Params
+from tqdm import tqdm
+
+
+# source from:https://stackoverflow.com/questions/2121874/python-pickling-after-changing-a-modules-directory
+class RenameUnpickler(pickle.Unpickler):
+    def find_class(self, module, name):
+        renamed_module = module
+        if module == "neuronal_motifs.server.models.neuron":
+            renamed_module = "models.neuron"
+
+        return super(RenameUnpickler, self).find_class(renamed_module, name)
+
+def renamed_load(file_obj):
+    return RenameUnpickler(file_obj).load()
+
+if __name__ == '__main__':
+    bucket = get_gcloud_storage_bucket()
+    if bucket:
+        base_path = "/Users/jinhan/Downloads/cache/cache/data/neurons1/"
+        local_paths = Path(base_path).glob("*.pkl")
+        for local_path in tqdm(local_paths, desc="Upload cache to GCloud"):
+            fname = str(local_path).split('/')[-1]
+
+            storage_path = Params.storage_root / "server" / "cache" / "data" / "neurons" / fname
+
+            with open(local_path, 'rb') as f:
+                data = renamed_load(f)
+                f.close()
+
+            with open(local_path,'wb') as f:
+                pickle.dump(data, f)
+                blob = bucket.blob(str(storage_path))
+                blob.upload_from_filename(local_path)
+
+
+
