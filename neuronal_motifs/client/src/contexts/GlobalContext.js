537ed1b216c2edc4137c6c09c8762aaf6f70e9e6
Jakob Troidl <jakob.troidl@googlemail.com>
2022-10-12 17:40:59 -0400

    add error message to context

--- neuronal_motifs/client/src/contexts/GlobalContext.js
+++ neuronal_motifs/client/src/contexts/GlobalContext.js
@@ -1,47 +1,47 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 
 export const AppContext = React.createContext(null);
 
 // Step 2: Create a ContextWrapper component that has to be the parent of every consumer.
 
 export const ContextWrapper = (props) => {
-    const [globalMotifIndex, setGlobalMotifIndex] = useState(0);
-    const [selectedMotifs, setSelectedMotifs] = useState([]);
-    const [focusedMotif, setFocusedMotif] = useState(null);
-    const [motifQuery, setMotifQuery] = useState();
-    const [abstractionLevel, setAbstractionLevel] = useState();
-    const [neuronColors, setNeuronColors] = useState([
-        "#7e2fd0",
-        "#81D02F",
-        "#34AFCB",
-        "#CB5034",
-        "#B3A94C",
-        "#4C56B3",
-        "#D9C226",
-        "#263DD9",
-    ]);
-    const [synapseColors, setSynapseColors] = useState([
-        "#FF6347",
-        "#DAA520",
-        "#97d0b5",
-        "#76acf3",
-        "#9400D3",
-        "#4C56B3",
-        "#D9C226",
-        "#263DD9",
-    ]);
+  const [globalMotifIndex, setGlobalMotifIndex] = useState(0);
+  const [selectedMotifs, setSelectedMotifs] = useState([]);
+  const [focusedMotif, setFocusedMotif] = useState(null);
+  const [motifQuery, setMotifQuery] = useState();
+  const [abstractionLevel, setAbstractionLevel] = useState();
+  const [neuronColors, setNeuronColors] = useState([
+    "#7e2fd0",
+    "#81D02F",
+    "#34AFCB",
+    "#CB5034",
+    "#B3A94C",
+    "#4C56B3",
+    "#D9C226",
+    "#263DD9",
+  ]);
+  const [synapseColors, setSynapseColors] = useState([
+    "#FF6347",
+    "#DAA520",
+    "#97d0b5",
+    "#76acf3",
+    "#9400D3",
+    "#4C56B3",
+    "#D9C226",
+    "#263DD9",
+  ]);
 
-    const [highlightColor, setHighlightColor] = useState("#0000ff");
+  const [highlightColor, setHighlightColor] = useState("#0000ff");
 
-    const [loadingMessage, setLoadingMessage] = useState();
-    const [errorMessage, setErrorMessage] = useState();
-    const [selectedSketchElement, setSelectedSketchElement] = useState(null);
-    const [resetUICounter, setResetUICounter] = useState(0);
+  const [loadingMessage, setLoadingMessage] = useState();
+  const [errorMessage, setErrorMessage] = useState();
+  const [selectedSketchElement, setSelectedSketchElement] = useState(null);
+  const [resetUICounter, setResetUICounter] = useState(0);
 
-    // motif occurences stats
-    const [absMotifCount, setAbsMotifCount] = useState(null); // count of a motif in the dataset, e.g motif1 occurs 100 times in the dataset
-    const [relativeMotifCount, setRelativeMotifCount] = useState(null); // count of all motifs in the dataset. e.g, motif1 is Highly represented in the dataset
-    const [showWarning, setShowWarning] = useState(false);
+  // motif occurences stats
+  const [absMotifCount, setAbsMotifCount] = useState(null); // count of a motif in the dataset, e.g motif1 occurs 100 times in the dataset
+  const [relativeMotifCount, setRelativeMotifCount] = useState(null); // count of all motifs in the dataset. e.g, motif1 is Highly represented in the dataset
+  const [showWarning, setShowWarning] = useState(false);
 
   // neighborhood query results
   const [neighborhoodQuery, setNeighborhoodQuery] = useState(null);
@@ -91,6 +91,8 @@ export const ContextWrapper = (props) => {
         setCurrentNeuronLabels,
         motifPathPosition,
         setMotifPathPosition,
+        errorMessage,
+        setErrorMessage,
       }}
     >
       {props.children}
