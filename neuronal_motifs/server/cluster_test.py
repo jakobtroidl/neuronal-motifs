a7f52cad368dc1445716d0f23358e3113d9a18b0
Jakob Troidl <jakob.troidl@googlemail.com>
2022-11-01 11:03:43 -0400

    (#76) experimenting with hierarchical clustering

--- neuronal_motifs/server/cluster_test.py
+++ neuronal_motifs/server/cluster_test.py
@@ -1,32 +1,18 @@
 from scipy.cluster.hierarchy import linkage
 import numpy as np
+from sklearn.cluster import AgglomerativeClustering
+import matplotlib.pyplot as plt
 
-a = np.random.multivariate_normal([10, 0], [[3, 1], [1, 4]], size=[100, ])
-b = np.random.multivariate_normal([0, 20], [[3, 1], [1, 4]], size=[50, ])
-centers = np.concatenate((a, b), )
-
-
-def create_tree(centers):
-    clusters = {}
-    to_merge = linkage(centers, method='single')
-    for i, merge in enumerate(to_merge):
-        if merge[0] <= len(to_merge):
-            # if it is an original point read it from the centers array
-            a = centers[int(merge[0]) - 1]
-        else:
-            # otherwise read the cluster that has been created
-            a = clusters[int(merge[0])]
-
-        if merge[1] <= len(to_merge):
-            b = centers[int(merge[1]) - 1]
-        else:
-            b = clusters[int(merge[1])]
-        # the clusters are 1-indexed by scipy
-        clusters[1 + i + len(to_merge)] = {
-            'children': [a, b]
-        }
-        # ^ you could optionally store other info here (e.g distances)
-    return clusters
+# generate random np array of 1000 rows and 3 columns
+X = np.random.rand(10000, 2)
 
 
-print(create_tree(centers))
+for n_clusters in range(2, 10):
+    clustering = AgglomerativeClustering(n_clusters=n_clusters)
+
+    # Visualizing the clustering
+    fig = plt.figure(figsize=(6, 6))
+
+    plt.scatter(X[:, 0], X[:, 1], c=clustering.fit_predict(X), cmap='rainbow')
+
+    plt.show()
